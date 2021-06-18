6ed26d7bf938c00c68f3bee2c3aa2b4671df85ba
conformist89 <olhalavoryk@gmail.com>
2021-06-18 10:30:11 +0300

    Inheritance with overloading was added

---
+++ lavoryk/to-exam/overload1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack{
+protected:
+
+  enum{MAX = 3;}  //stack size
+  int st[MAX];
+  int top;      // last element index
+
+public:
+  Stack()       // constructor
+  { top = -1; }
+
+  void push(int var)
+  {st[++top] = var;}
+
+  int pop()
+  {return st [top--]; }
+
+};
+
+class Stack2 :  public Stack{
+
+public:
+  void push (int var){
+      if(top >= MAX - 1){
+         cout  <<   "\nError: stack overflow"; <<endl;
+      }
+  }
+
+  int pop()  {
+    if(top < 0){
+      cout  <<   "\nError: stack is empty"; <<endl;
+    }
+  }
+
+};
+
+int main(){
+  Stack2  s1;
+
+  s1.push(11);
+  s1.push(22);
+  s1.push(33);
+
+  cout << endl << s1.pop () << endl;
+  cout << endl << s1.pop () << endl;
+  cout << endl << s1.pop () << endl;
+  cout << endl << s1.pop () << endl;
+  cout<< endl;
+}
